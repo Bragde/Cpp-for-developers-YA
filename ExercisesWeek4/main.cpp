b1afb0684c739521d7cace2a1d374cdd347daf7e
Daniel Lundholm <daniel.lundholm@dekiru.se>
2023-11-08 17:43:11 +0100

    exercises week 4 task 3

--- ExercisesWeek4/main.cpp
+++ ExercisesWeek4/main.cpp
@@ -1,5 +1,6 @@
 #include <string>
 #include <iostream>
+#include <vector>
 using namespace std;
 
 struct Student
@@ -87,52 +88,109 @@ struct Student
 
 //  EXERCISE 2
 #pragma region Exercise_2
-void InputStudent(Student &p)
-{
-    cout << "Firstname: ";
-    cin >> p.firstName;
-    cout << "Lastname: ";
-    cin >> p.lastName;
-    cout << "City: ";
-    cin >> p.city;
-    cout << "Birthyear: ";
-    cin >> p.birthYear;
-}
+// void InputStudent(Student &p)
+// {
+//     cout << "Firstname: ";
+//     cin >> p.firstName;
+//     cout << "Lastname: ";
+//     cin >> p.lastName;
+//     cout << "City: ";
+//     cin >> p.city;
+//     cout << "Birthyear: ";
+//     cin >> p.birthYear;
+// }
 
-void PrintStudent(const Student &p)
+// void PrintStudent(const Student &p)
+// {
+//     cout << "Firstname: " << p.firstName << "\nLastname: " << p.lastName << "\nCity: " << p.city << "\nBirthyear: " << p.birthYear << endl;
+// }
+
+// int main()
+// {
+//     Student room[10];
+//     auto nrStudents = 0;
+
+//     // Enter students into the classroom
+//     for (int count = 0; count < (int)size(room); count++)
+//     {
+//         cout << "Do you want to add a student? (yes/no) ";
+//         string enterMore = "no";
+//         cin >> enterMore;
+//         if (enterMore != "yes")
+//             break;
+
+//         Student student;
+//         InputStudent(student);
+//         room[count] = student;
+//         nrStudents++;
+//     }
+
+//     // Find the oldest student
+//     Student oldestStudent;
+//     oldestStudent.birthYear = 0;
+//     for (int count = 0; count < nrStudents; count++)
+//     {
+//         if (room[count].birthYear > oldestStudent.birthYear)
+//             oldestStudent = room[count];
+//     }
+//     cout << "The oldest student is:" << endl;
+//     PrintStudent(oldestStudent);
+// }
+#pragma endregion Exercise_2
+
+// EXERCISE 3
+#pragma region Exercise_3
+enum DishType
 {
-    cout << "Firstname: " << p.firstName << "\nLastname: " << p.lastName << "\nCity: " << p.city << "\nBirthyear: " << p.birthYear << endl;
-}
+    Vegiterian,
+    Vegan,
+    Meat
+};
+
+class Dish
+{
+public:
+    string name;
+    float price;
+    DishType type;
+    int nrOfCalories;
+
+    void Print()
+    {
+        auto type =
+            this->type == Vegiterian ? "Vegiterian"
+            : this->type == Vegan    ? "Vegan"
+            : this->type == Meat     ? "Meat"
+                                     : "Dishtype undefined";
+        cout << this->name << "\t" << this->price << " sek\n"
+             << type << "\t" << this->nrOfCalories << " calories\n\n";
+    }
+};
 
 int main()
 {
-    Student room[10];
-    auto nrStudents = 0;
+    vector<Dish> todaysLunch;
 
-    // Enter students into the classroom
-    for (int count = 0; count < (int)size(room); count++)
-    {
-        cout << "Do you want to add a student? (yes/no) ";
-        string enterMore = "no";
-        cin >> enterMore;
-        if (enterMore != "yes")
-            break;
+    Dish dish_1;
+    dish_1.name = "Pasta with cheese and mushrooms";
+    dish_1.price = 113;
+    dish_1.type = Vegiterian;
+    dish_1.nrOfCalories = 1000;
+    todaysLunch.push_back(dish_1);
+    Dish dish_2;
+    dish_2.name = "Grass with dry dirt and localy produces mudd";
+    dish_2.price = 999;
+    dish_2.type = Vegan;
+    dish_2.nrOfCalories = 1;
+    todaysLunch.push_back(dish_2);
+    Dish dish_3;
+    dish_3.name = "Violently slughtered unicorn bathing in fresh blood";
+    dish_3.price = 250;
+    dish_3.type = Meat;
+    dish_3.nrOfCalories = 2000;
+    todaysLunch.push_back(dish_3);
 
-        Student student;
-        InputStudent(student);
-        room[count] = student;
-        nrStudents++;
-    }
-
-    // Find the oldest student
-    Student oldestStudent;
-    oldestStudent.birthYear = 0;
-    for (int count = 0; count < nrStudents; count++)
-    {
-        if (room[count].birthYear > oldestStudent.birthYear)
-            oldestStudent = room[count];
-    }
-    cout << "The oldest student is:" << endl;
-    PrintStudent(oldestStudent);
+    for (auto d : todaysLunch)
+        d.Print();
 }
-#pragma endregion Exercise_2
+#pragma endregion Exercise_3
